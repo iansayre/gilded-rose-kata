266789332429248523b3295de18f064cd5a3d669
Ian Sayre <me@iansayre.com>
2018-08-12 23:10:25 -0700

    I extended the item class to include a boolean for isConjured and added condintional statements looking for that variable

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,9 +1,15 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  // it("should foo", function() {
+  //   const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toEqual("fixme");
+  // });
+
+  it ('should degrade twice as fast if item is conjured', function() {
+    const gildedRose = new Shop([ new Item('cool pants', 10, 30), new conjuredItem('knockoff pants', 10, 30, true)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
-  });
+    expect(items[1].quality).toBeLessThan(items[0].quality);
+  })
 
 });
--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,6 +6,13 @@ class Item {
   }
 }
 
+class conjuredItem extends Item {
+  constructor(name, sellIn, quality, isConjured) {
+    super(name, sellIn, quality);
+    this.isConjured = isConjured;
+  }
+}
+
 class Shop {
   constructor(items=[]){
     this.items = items;
@@ -15,7 +22,12 @@ class Shop {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+            if (this.items[i].isConjured) {
+              this.items[i].quality = this.items[i].quality - 2;
+            }
+            else {
+              this.items[i].quality = this.items[i].quality - 1;
+            }
           }
         }
       } else {
@@ -35,15 +47,22 @@ class Shop {
           }
         }
       }
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
+                if (this.items[i].isConjured) {
+                  this.items[i].quality = this.items[i].quality - 2;
+                }
+                else {
+                  this.items[i].quality = this.items[i].quality - 1;
+                }
               }
             }
           } else {
